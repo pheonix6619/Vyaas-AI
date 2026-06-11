ecb9a6cc78d4580ead7ca422ddd8c4d22a681647
pheonix6619 <chaithanyakanchanapally2.0@gmail.com>
2026-06-12 00:34:30 +0530

    Delete read_lines.ps1

--- read_lines.ps1
+++
@@ -1,6 +0,0 @@
-$lines = [System.IO.File]::ReadAllLines('E:\Made_with_ai\Project_1\lib\ui\chat_screen.dart')
-for ($idx = 733; $idx -le 741; $idx++) {
-    $line = $lines[$idx]
-    $num = $idx + 1
-    Write-Host "${num}: |${line}|"
-}
